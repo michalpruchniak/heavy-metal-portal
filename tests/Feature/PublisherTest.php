455c6ab3607fe1c9d5aad73094acc10afbf6cf6a
Michal <goldenofheart@gmail.com>
2025-05-14 23:01:40 +0200

    test

--- tests/Feature/PublisherTest.php
+++ tests/Feature/PublisherTest.php
@@ -3,15 +3,15 @@
 use App\Models\Publisher;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Permission\Models\Role;
 
 uses(RefreshDatabase::class);
 
 beforeEach(function () {
-    // Uruchom migracje przed każdym testem
     $this->artisan('migrate');
 
-    // Tworzenie ról
     Role::firstOrCreate(['name' => 'admin']);
     Role::firstOrCreate(['name' => 'moderator']);
 });
@@ -89,4 +89,44 @@ test('publisher edit page is disabled for guests and users without role', functi
     $this->actingAs($user)
         ->get(route('publishers.edit', ['publisher' => $publisher->id]))
         ->assertForbidden();
+});
+
+test('new publisher can be added', function () {
+    $admin = User::factory()->create();
+    $admin->assignRole('admin');
+
+    Storage::fake('public');
+
+    $image = UploadedFile::fake()->image('logo.jpg');
+
+    $response = $this->actingAs($admin)->post(route('publishers.store'), [
+        'name' => 'Test Publisher',
+        'logo' => $image,
+        'url' => 'http://testpublisher.com',
+        'description' => 'Test description of the publisher that is within the character limits.',
+    ]);
+
+    $response->assertRedirect(route('publishers.index'));
+
+    $this->assertDatabaseHas('publishers', [
+        'name' => 'Test Publisher',
+        'url' => 'http://testpublisher.com',
+        'description' => 'Test description of the publisher that is within the character limits.',
+    ]);
+});
+
+test('publisher validation test', function () {
+    $admin = User::factory()->create();
+    $admin->assignRole('admin');
+    $faker = Faker\Factory::create();
+
+    $response = $this
+        ->actingAs($admin)
+        ->post(route('publishers.store'), [
+            'logo' => null,
+            'url' => 'wrong-utl',
+            'description' => $faker->words(2500, true),
+        ]);
+
+    $response->assertSessionHasErrors(['name', 'logo', 'description']);
 });
