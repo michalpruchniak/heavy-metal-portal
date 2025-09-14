5281a98b797177db85c0e51a8d652b3840ed48ce
Michal <goldenofheart@gmail.com>
2025-09-14 13:19:41 +0200

    add abserver

---
+++ app/Observers/EventObserver.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Event;
+use Illuminate\Support\Facades\Cache;
+
+class EventObserver
+{
+    /**
+     * Handle the Event "created" event.
+     */
+    public function created(Event $event): void
+    {
+        Cache::forget('events_all');
+
+    }
+
+    /**
+     * Handle the Event "updated" event.
+     */
+    public function updated(Event $event): void
+    {
+        Cache::forget('events_all');
+
+    }
+
+    /**
+     * Handle the Event "deleted" event.
+     */
+    public function deleted(Event $event): void
+    {
+        Cache::forget('events_all');
+    }
+
+    /**
+     * Handle the Event "restored" event.
+     */
+    public function restored(Event $event): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Event "force deleted" event.
+     */
+    public function forceDeleted(Event $event): void
+    {
+        //
+    }
+}
